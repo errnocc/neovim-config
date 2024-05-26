5cc7737b4a6d3bab75163250a3c86f4b1c48f8cc
errnocc <2228972546@qq.com>
2024-05-26 16:53:53 +0800

    add-the lsp diagnostic of keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("n", "<leader>ej", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
+map("n", "<leader>ek", vim.diagnostic.goto_prev, { desc = "Goto next diagnostic" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
